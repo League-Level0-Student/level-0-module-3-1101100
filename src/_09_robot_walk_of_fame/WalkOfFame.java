1365a8c7b0948fe6274354d39c117bd4eb092dd7
1101100 <joshtang255@gmail.com>
2019-10-12 19:37:57 -0700

    finished walk of fame

--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -13,14 +13,24 @@ public class WalkOfFame {
 		rob.setSpeed(200);
 		rob.miniaturize();
 		// 1. Set the X position of the robot so that it starts on the left. 
-		rob.setX(10);
-		int x = 10;
+		rob.setX(50);
+		int x = 50;
 		// You also need to show the robot to see the result of this line.
 		
 		// 2. Make the robot draw a star shape. Hint: 144.
 
 		// 3. Set the size of the star to 30.
 
+		for (int b = 0; b<10; b++) {
+			rob.penDown();
+			for (int i = 0; i < 5; i++) {
+			rob.move(starSize);
+			rob.turn(144);
+			}
+			x +=starSize+20;
+			rob.setX(x);
+	
+		}
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
@@ -31,12 +41,6 @@ public class WalkOfFame {
 		
 
 	}
-	private void drawStar(int starSize) {
-		rob.penDown();
-		for (int i = 0; i < 5; i++) {
-			rob.move(starSize);
-	rob.turn(144);
-	}
-	}
 
 }
+
